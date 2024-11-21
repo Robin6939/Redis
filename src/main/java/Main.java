f416f400e5b39372e6dfe0d435acb7901bb85648
Robin6939 <robinraj6939@gmail.com>
2024-11-21 13:12:39 +0530

    Listensing for PING and returning PONG

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,12 @@
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
   public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
 
-    //  Uncomment this block to pass the first stage
     ServerSocket serverSocket = null;
     Socket clientSocket = null;
     int port = 6379;
@@ -18,6 +17,8 @@ public class Main {
       serverSocket.setReuseAddress(true);
       // Wait for connection from client.
       clientSocket = serverSocket.accept();
+      OutputStream outputStream = clientSocket.getOutputStream();
+      outputStream.write("+PONG\r\n");
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     } finally {
