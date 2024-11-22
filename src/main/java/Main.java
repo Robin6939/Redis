0281490e522aadf484c4035679945f06e02127e7
Robin6939 <robinraj6939@gmail.com>
2024-11-22 21:07:30 +0530

    implemented parser for echo command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -10,13 +11,67 @@ public class Main extends Thread  {
   static Vector<Socket> v = new Vector<>();
   static int size = 0;
 
+  public void readCommand(InputStream in, Vector<String> command) throws IOException {
+    int x = in.read() - (int)'0';
+    // System.out.println("Size of the command: "+x);
+    while(x-->0) {
+      int skip = 5;
+      while(skip-->0) {
+        // System.out.println("Skip");
+        in.read();
+      }
+      int y = in.read() - (int)'0';
+      // System.out.println("Lenght of the next element in the command vector: "+y);
+      skip = 4;
+      while(skip-->0) {
+        // System.out.println("Skip");
+        in.read();
+      }
+      String s="";
+      while(y-->0) {
+        s=s+(char)in.read();
+      }
+      // System.out.println(s);
+      command.addElement(s);
+    }
+    int skip = 4;
+    while(skip-->0) {
+      // System.out.println("Skip");
+      in.read();
+    }
+  }
+
+  public String encodeRESP(String s) {
+    int size = s.length();
+    String ret = "$" + size + "\\r\\n" + s + "\\r\\n";
+    return ret;
+  }
+
   public void run() {
     Socket s = getSocket();
     try (InputStream in = s.getInputStream()) {
-      OutputStream out = s.getOutputStream();
+      DataOutputStream out = new DataOutputStream(s.getOutputStream());
       while(true) {
-        if(in.read()==42) {
-          out.write("+PONG\r\n".getBytes());
+        in.read();
+        in.read();
+        char ch = (char)in.read();
+        // System.out.println(ch);
+        if(ch=='*') {
+          Vector<String> command = new Vector<>();
+          readCommand(in, command);
+          System.out.println(command);
+          if(command.get(0).equalsIgnoreCase("ECHO")) {
+            System.out.println("It is an ECHO command");
+            String send = encodeRESP(command.get(1));
+            out.writeUTF(send);
+          }
+          if(command.get(0).equalsIgnoreCase("PING")) {
+            System.out.println("It is an PING command");
+            out.writeUTF("+PONG\r\n");
+          }
+        }
+        else {
+          System.out.println("This is the incorrect Command");
         }
       }
     } catch (IOException e) {
@@ -46,20 +101,9 @@ public class Main extends Thread  {
     try {
       serverSocket = new ServerSocket(port);
       serverSocket.setReuseAddress(true);
-      // System.out.println("Tyring to connect");
-      // clientSocket = serverSocket.accept();
-      // System.out.println("Connection established");
-      // InputStream inputStream = (clientSocket.getInputStream());
-      // OutputStream outputStream = clientSocket.getOutputStream();
-      // while(true) {
-      //   int line = inputStream.read();
-      //   System.out.println("Data received : " + line);
-      //   if(line==42)
-      //     outputStream.write("+PONG\r\n".getBytes());
-      // }
-
       while(true) {
         clientSocket = serverSocket.accept();
+        System.out.println("Connection established");
         addSocket(clientSocket);
         Main t = new Main();
         t.start();
